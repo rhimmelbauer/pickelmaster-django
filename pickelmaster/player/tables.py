c7f5c364cb01d6e322135b398688a0dae7f8c2c2
Roberto Himmelbauer <robertoh89@gmail.com>
2023-12-13 12:59:20 -0800

    Added render_name to table to redirect to Player profile and created PartnerLoseOrWinCounterTable

--- pickelmaster/player/tables.py
+++ pickelmaster/player/tables.py
@@ -1,4 +1,6 @@
 import django_tables2 as tables
+from django.utils.html import format_html
+from django.urls import reverse
 
 
 class PlayerTable(tables.Table):
@@ -15,6 +17,12 @@ class PlayerTable(tables.Table):
 
     def render_ratio(self, value):
         return "{:0.2f}%".format(value)
+    
+    def render_name(self, value):
+        url = reverse('player', kwargs={'username': value})
+        return format_html(
+            f"<a href={url}>{value}</a>"
+        )
 
 
 class PlayerXWinningCountTable(tables.Table):
@@ -28,4 +36,25 @@ class PlayerXWinningCountTable(tables.Table):
 
     def render_ratio(self, value):
         return "{:0.2f}%".format(value)
+    
+    def render_name(self, value):
+        url = reverse('player', kwargs={'username': value})
+        return format_html(
+            f"<a href={url}>{value}</a>"
+        )
 
+
+class PartnerLoseOrWinCounterTable(tables.Table):
+    name = tables.Column(verbose_name="Name", orderable=False)
+    counter = tables.Column(verbose_name="Count", orderable=True)
+    
+    class Meta:
+        template_name = "includes/custom_dt2_bootstrap4.html"
+        fields = ("name", "counter")
+        order_by = ['-counter']
+    
+    def render_name(self, value):
+        url = reverse('player', kwargs={'username': value})
+        return format_html(
+            f"<a href={url}>{value}</a>"
+        )
